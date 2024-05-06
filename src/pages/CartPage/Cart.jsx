7e21d1adc0aa2d6aed1e8a84e78e25dd0b0b08de
mandar-saiyan87 <mdman2257@gmail.com>
2024-05-06 18:51:12 +0530

    show products in cart

--- src/pages/CartPage/Cart.jsx
+++ src/pages/CartPage/Cart.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { addtocart, removefromcart } from '../../store/cartSlice'
+import { assets } from '../../assets/assets'
 
 function Cart() {
 
   const cartItems = useSelector((state) => state.cart.cartItems)
 
-  console.log(cartItems)
+  const dispatch = useDispatch()
+
+  // console.log(cartItems)
+
+  function addQty(item) {
+    // console.log(item)
+    dispatch(addtocart({ 'menuItem': item, 'qty': 1 }))
+
+  }
+
+  function reduceqty(item) {
+    // console.log(item)
+    dispatch(removefromcart({ 'menuItem': item, 'qty': -1 }))
+  }
 
   return (
     <>
@@ -28,15 +42,19 @@ function Cart() {
             <input type="number" id='phone' name='phone' placeholder='phone' />
           </form>
         </Container>
-        <Container className='text-center'>
+        <Container className=''>
           <h5>Cart Total</h5>
           <div>
             {cartItems.length > 0 ?
               cartItems.map(item => (
-                <div className='cart_item' >
+                <div className='cart_item' key={item.menuItem._id}>
                   <p className='title'>{item.menuItem.name}</p>
-                  <p className='qty'>{item.qty}</p>
-                  <p className='amount'>{item.menuItem.price}</p>
+                  <div className='manageqty'>
+                    <img src={assets.remove_icon_red} alt="removeqty" className='dcard_ico' onClick={() => reduceqty(item.menuItem)} />
+                    <p className='qty'>{item.qty}</p>
+                    <img src={assets.add_icon_green} alt="addqty" className='dcard_ico' onClick={() => addQty(item.menuItem)} />
+                  </div>
+                  <p className='amount'>${item.menuItem.price}</p>
                 </ div>
               )) :
               <div>No items added to cart</div>
