08c937451d6f62ab6ece5dc7cb90de9dd8ceb602
mandar-saiyan87 <mdman2257@gmail.com>
2024-05-06 08:27:26 +0530

    mobile navbar in progress

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -87,9 +87,9 @@ function Navbar() {
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
       </div>
-      <div className='mobile_menu-drawer'>
+      {/* <div className='mobile_menu-drawer'>
 
-      </div>
+      </div> */}
     </div >
   )
 }
