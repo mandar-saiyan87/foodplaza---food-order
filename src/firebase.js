ed02c2d0254e417fa7fd0136c0cf046354a5ac1d
mandar-saiyan87 <mdman2257@gmail.com>
2024-12-10 12:12:44 +0530

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-
 
-const apiKey = process.env.REACT_APP_FIREBASE_APIKEY;
+const apiKey = process.env.REACT_APP_FIREBASE_API;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
